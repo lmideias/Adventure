5ea2748f4e3e6852c0e41c28b75281d5d1fc5c1e
LucianoEun <lucianoeun@hotmail.com>
2016-06-15 15:18:30 -0300

    Criação da pagina adventure TV em Ipanema PC

---
+++ Views/tv/tv.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Adventure TV</title>
+        <link href="tv.css" rel="stylesheet"/>
+        <link href="../sobre/Reset.css" rel="stylesheet"/>
+        
+    </head>
+    <body class="beckground">
+       <div class="barra-total">  
+            <div class="barra-central"> 
+                     <div class="bara-logo">                          
+                         <img alt="Logo" src="../sobre/adventure-min.png" width="60" height="58" />
+                     </div>  
+                     
+                   <div class="barra-menu"> 
+                     
+                  </div> 
+             </div>      
+       </div>
+       
+        
+        <div class="barra-total-prop">  
+            <div class="barra-central-prop"> 
+                <h1>Propaganda</h1>
+             </div>      
+       </div>
+        
+        
+        <div class="main-body">  
+          <div class="tv-container"> 
+               <iframe frameborder="0" width="100%" height="800" src="//www.dailymotion.com/embed/video/x3fi6gf" allowfullscreen></iframe><br /><a href="http://www.dailymotion.com/video/x3fi6gf_geordie-shore-season-11-episode-6-geordie-shore-s11e06-geordieshore-1080p-full-episodes_shortfilms" target="_blank">Geordie Shore Season 11 Episode 6 - Geordie...</a> <i>por <a href="http://www.dailymotion.com/tvshow2015" target="_blank">tvshow2015</a></i>
+              
+          </div> 
+            <hr />       
+            <div class="sobre-container"> 
+                <h1>Ao vivo</h1>
+            </div>  
+             <hr />       
+            <div class="sobre-container"> 
+              <h1>Videos</h1>
+            </div> 
+       </div>
+        <div class="footer">  
+            <div class="container"> 
+               <h3>Textos para analise de espaço</h3> 
+            </div>       
+       </div>
+    </body>
+</html>
