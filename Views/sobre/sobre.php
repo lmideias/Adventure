f65f3518189c7a46308a88693648e55671f01237
LucianoEun <lucianoeun@hotmail.com>
2016-06-16 00:56:12 -0300

    Reformulação do layout da pagina sobre, no pc de casa

--- Views/sobre/sobre.php
+++ Views/sobre/sobre.php
@@ -3,47 +3,77 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta property="og:http://trilhasadventure.rf.gd">
         <title>Sobre</title>
-        <link href="sobre.css" rel="stylesheet"/>
+        <link href="ssobre.css" rel="stylesheet"/>
         <link href="../Content/main.css" rel="stylesheet"/>
         <link href="Reset.css" rel="stylesheet"/>
         
     </head>
-    <body class="bg-body">
+    <body class="bg-body">     
+   
+            
+       <div class="barra-total-1"></div>
        <div class="barra-total">  
             <div class="barra-central"> 
-                     <div class="bara-logo">                          
+                 <div class="container-a"> 
+                     <div class="barra-logo">                          
                          <img alt="Logo" src="adventure-min.png" width="60" height="58" />
                      </div>  
-                     
-                   <div class="barra-menu"> 
-                    <a href="/index.php"><div class="home-btn">Home</div></a>
-                    <a href="Views/CadastroUsuario.php"><div class="home-btn">Cadastro</div></a>
-                    <a href="Admin.php"><div class="home-btn">Admin</div></a>
-                  </div> 
+                     <div class="barra-social"> 
+                          <div class="fb-like" data-href="http://trilhasadventure.rf.gd" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
+                      </div> 
+                      <div class="barra-menu"> 
+                          <ul>
+                              <li><a href="">Sair</a></li>
+                              <li><a href="">Menu 02</a></li>
+                              <li><a href="">Home</a></li>
+                          </ul>
+                      </div> 
+                 </div> 
              </div>      
        </div>
        
         <div class="main-body">  
-             <div class="box-body"> 
-          <div class="sobre-container"> 
+           <div class="box-body"> 
+               <div class="container-texto"> 
                <h2>Quem e o Trilhas Adventure</h2> 
                <br />
-               <p>
-                   Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-               </p>
-          </div>  
-            <div class="sobre-container"> 
+               
+                  <p> Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature </p>
+                   <p>from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia,</p> 
+                   <p>looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of </p>
+                   <br />
+                   <p>the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 </p>
+                   <p>of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise </p>
+                   <p>on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet.</p>
+               
+               </div>  
+            
+            
+            
+               <div class="container-texto"> 
                <h2>Equipe Trilhas Adventure</h2> 
                <br />
-               <p>
-                  Contrary to popular belief, Lorem Ipsum is not simply random text.
-               </p>
+                <p>the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 </p>
+                   <p>of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise </p>
+                   <p>on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet.</p>
                
                
+                </div>  
+               <div class="container-texto"> 
+               <h2>Equipe Trilhas Adventure</h2> 
+               <br />
+                <p>the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 </p>
+                   <p>of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise </p>
+                   <p>on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet.</p>
                
                
-               <div class="box-user">
+                </div> 
+               
+               <br /><br />
+               
+               <div class="box-user mgt">
                     <div class="box-user-img">
                       <img alt="Logo" src="adventure-min.png" width="150" height="140" />
                    </div>
@@ -230,13 +260,28 @@
                </p>
                    </div>
                </div>
-          </div>  
+               
+               
+          </div> 
+            
+            
        </div> 
-       </div>
-        <div class="footer">  
-            <div class="container"> 
-               <h3>Textos para analise de espaço</h3> 
-            </div>       
-       </div>
+           <div class="footer">  
+                      <div class="container"> 
+                          <h3>Textos para analise de espaço</h3> 
+                      </div>       
+          </div> 
+        
+                <div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.6&appId=570611709655080";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+</script>       
+        
+     
     </body>
 </html>
