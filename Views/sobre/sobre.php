37d8298f2c8a84b8134e1103e0388dede59ff162
Luciano Oliveira <lucianoeun@hotmail.com>
2016-06-22 10:07:05 -0300

    private ipanema

--- Views/sobre/sobre.php
+++ Views/sobre/sobre.php
@@ -25,15 +25,26 @@
           <div class="sobre-container"> 
                <h2>Quem e o Trilhas Adventure</h2> 
                <br />
-               <p>
-                   Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+               <p>            
+
+
+Trilhas Adventure é um grupo voluntário que visa a integração do participante ao estilo de vida saudável, através da prática de atividades voltada para o ecoturismo, incluindo trilhas, esportes radicais, e aventuras em espaços naturais. E, ainda, diversos passeios e eventos voltados para área social, como arrecadação de agasalhos, alimentos, entre outros. 
+
+
+Como fazemos ?
+(nossos meios de organização)
+
+Nossa equipe é formada por pessoas experientes que tem o compromisso de proporcionar aventuras e momentos únicos com toda a segurança requerida para tais atividades.
+Os eventos são previamente organizados, os locais são visitados com antecedência para montagem do melhor roteiro de acordo com o público- alvo. 
+
+               
                </p>
           </div>  
             <div class="sobre-container"> 
                <h2>Equipe Trilhas Adventure</h2> 
                <br />
                <p>
-                  Contrary to popular belief, Lorem Ipsum is not simply random text.
+                  Contrary to Grupo popular belief, Lorem Ipsum is not simply random text.
                </p>
                
                
