e10e22e9721552a15132aa6fc106c09c47b83f13
LucianoEun <lucianoeun@hotmail.com>
2016-06-28 16:28:07 -0300

    Criação da pagina de cadastro usando a da pagina index em Ipanema PC

--- index.php
+++ index.php
@@ -1,111 +1,83 @@
-
 <!DOCTYPE html>
-
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        
         <title>Trilhas Adventure</title>
-        <link href="Content/main.css" rel="stylesheet"/>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> 
+        <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
+        <link rel="stylesheet" type="text/css"  href="Content/main.css" />
+              
+        
         <link href="Content/Reset.css" rel="stylesheet"/>
         <script src="Scripts/main.js"></script>
     </head>
     <body>
-        <div class="header-bc">  
-           <div class="h-social">
-                <div class="hs-01">
-                    <div class="fb-like" data-href="http://trilhasadventure.rf.gd" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
-                      
-                </div>
-                <div class="hs-02">
-                     <a href="#"><div class="home-btn">Participar</div></a>
-                     <a href="#"><div class="home-btn">Agenda</div></a>
-                     <a href="Views/sobre/sobre.php"><div class="home-btn">Sobre</div></a>      
-                </div>
-                <div class="hs-03">
-                    <a href="#"> <img alt="Logo" src="/Imagens/whats.png" width="50" height="50" /></a>
-                </div>
-                <div class="hs-03">
-                     <a href="#"><img alt="Logo" src="/Imagens/youtube.png" width="50" height="50" /></a>
-                </div>
-                <div class="hs-03">
-                     <a href="#"> <img alt="Logo" src="/Imagens/insta.png" width="50" height="50" /></a>
-                </div>
-                <div class="hs-03">
-                      <a href="#"> <img alt="Logo"  src="/Imagens/face.png" width="50" height="50" /></a>
-                </div>          
-           
-           </div>
-           <div class="h-body">
-                <div class="hb-left"></div>          
-                <div class="hb-center">
-                    <div class="hbc-logo">
-                     <img alt="Logo" src="/Imagens/adventure-min.png" width="250" height="230" />
-                    </div>
-                </div>
-                <div class="hb-right"></div>
-           </div>
-       </div>
-
-       
-        <div class="main-body">  
-            <div class="container-body"> 
-                 <div class="box-b1"> 
-                     
-                 </div>
-                 <div class="box-b2"> 
-                      <img alt="Logo" src="/Imagens/bg-min.png" width="600" height="400" />
-                 </div>
-                 <div class="box-b3"> 
-                     
-                 </div>
-                <!-- menu body-->
-                 <div class="b-menu"> 
-                      <div class="bm-op"> 
-                          <div class="bm-op-titulo"> 
-                              <h3> Agenda de Eventos</h3>
-                          </div>
-                          <div class="bm-op-imagem"> 
-                              <img alt="Logo" src="/Imagens/bg-min.png" width="180" height="120" />
-                          </div>
-                          <div class="bm-op-link"> 
-                              <h4>Link de acesso</h4>
-                          </div>
-                      </div>
-                 </div>
+        <div class="ca-body">
+            <div class="ca-header">
+                  <div class="ca-logo">
+                       <img alt="Logo" src="/Imagens/adventure-min.png" width="80" height="76" />
+                  </div>
+                   <div class="ca-menu">
+                       <ul>
+                           <li><a href=""><h2>Cadastrar</h2></a></li>
+                           <li><a href=""><h1>Cadastrados</h1></a></li>                           
+                       </ul>
+                  </div>
+            </div>
+            <div class="ca-container">
+                  <br /><br />
+                <h3>Cadastro para Viagem</h3> <br /> <br />
+                <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                </p>
+                <br /><br />
+                
+                <h3>Documento de Identificação</h3>
+                <br /><br />
+                <label>Nome Completo</label><p/>
+                <input type="text" value="numero"/>
+                <br /><br />
+                <label>Telefone</label><p/>
+                <input type="text" value="numero"/>
+                <br /><br />
                 
                 
-                 <div class="box-a"> 
-                 Trilhas Adventure é um grupo voluntário que visa a integração do participante ao estilo de vida saudável, 
-                  <p> através da prática de atividades voltada para o ecoturismo, incluindo trilhas, esportes radicais, e aventuras
-                   em espaços naturais. </p><br />
-                   <p> E, ainda, diversos passeios e eventos voltados para área social, como arrecadação de agasalhos,
-                 alimentos, entre outros. </p>
-                   <br />            
-               
-                 <p>Nossa equipe é formada por pessoas experientes que tem o compromisso de proporcionar aventuras e momentos únicos com toda a segurança requerida para tais atividades.</p><br />
-                 <p>Os eventos são previamente organizados, os locais são visitados com antecedência para montagem do melhor roteiro de acordo com o público- alvo. </p>
-                 </div>
-            </div>  
-       
-       </div>
-        
-        
-        
-        <div class="footer">  
-            <div class=""> 
-               <h3>Textos para analise de espaço</h3> 
-            </div>       
-       </div>
-        
-       <div id="fb-root"></div>
-             <script>(function(d, s, id) {
-             var js, fjs = d.getElementsByTagName(s)[0];
-             if (d.getElementById(id)) return;
-             js = d.createElement(s); js.id = id;
-             js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.6&appId=570611709655080";
-             fjs.parentNode.insertBefore(js, fjs);
-             }(document, 'script', 'facebook-jssdk'));
-            </script> 
-
+                <h3>Documento de Identificação</h3>
+                 <br /><br />
+                 <label>Numero</label><p/>
+                <input type="text" value="numero"/>
+                 <br /><br />
+                 
+                <label>Orgão Emissor</label><p/>
+                <input type="text" value="numero"/>
+                 <br /><br />
+                 <label>Data expedição</label><p/>
+                <input type="text" value="numero"/>                
+                 <br /><br />
+                 
+                <label>UF</label><p/>
+                <input type="text" value="numero"/>                
+                  <br />
+                   <br />
+                
+                 <h3>Contato de Emergência</h3>
+                <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                </p>
+                <br /><br />
+                 
+                <label>Nome Completo</label><p/>
+                <input type="text" value="numero"/>
+                 <br /><br />
+                 <label>Telefone</label><p/>
+                <input type="text" value="numero"/>                
+                 <br /><br />
+            </div>
+        </div>
     </body>
 </html>
