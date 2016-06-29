af1186b97cc950bc0885c383e60bb3e0c93e3d0b
LucianoEun <lucianoeun@hotmail.com>
2016-06-29 10:21:25 -0300

    criação de paginas adm

---
+++ admpage.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+        <title>Trilhas Adventure</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> 
+        <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
+        <link rel="stylesheet" type="text/css"  href="Content/main.css" />
+        <link rel="stylesheet" type="text/css"  href="Content/bootstrap.css" />     
+        
+        <link href="Content/Reset.css" rel="stylesheet"/>
+        <script src="Scripts/main.js"></script>
+    </head>
+    <body>
+        <div class="ca-body">
+            <div class="ca-header">
+                  <div class="ca-logo">
+                      <a href="admlogin.php"><img alt="Logo" src="/Imagens/adventure-min.png" width="80" height="76" /></a>
+                  </div>
+                   <div class="ca-menu">
+                       <div class="ca-titulo">
+                           <h1>Cadastros</h1>
+                      </div>
+                        <div class="ca-numero">
+                        100
+                       </div>
+                  </div>
+            </div>
+            <div class="ca-container">
+           
+                
+            </div>
+        </div>
+    </body>
+</html>
