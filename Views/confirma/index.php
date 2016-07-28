c5a9d5bb519785192fe837f0d611bb4480b458ba
LucianoEun <lucianoeun@hotmail.com>
2016-07-28 07:59:46 -0300

    criação do sistema de confirmação em casa pc

---
+++ Views/confirma/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html ng-app="inApp">
+    <head>
+        <meta charset="UTF-8">
+         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Participantes</title>       
+        <link rel="stylesheet" type="text/css"  href="content/confirma.css" />
+        <link rel="stylesheet" type="text/css"  href="/Content/Reset.css" />
+    </head>
+    <body class="body-container"> 
+        
+        <div class="menu-barra">
+            <div class="menu-logo">
+                    <img alt="Logo" src="content/" width="200" height="200" />
+            </div>
+        </div>
+         
+        <div class="hbc-logo">
+            <img alt="Logo" src="content/logo.png"  />
+        </div>
+        <div class="infor">
+            <h4>Participantes</h4>
+            <br />
+            <h1>100</h1>
+        </div>   
+
+
+    </body>
+</html>
---
+++ Views/confirma/templates/links.php
@@ -0,0 +1,18 @@
+<!-- Links externos  -->
+
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.6/angular.min.js"></script>       
+<script src="//cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.8/angular-ui-router.min.js"></script>
+<script type="text/javascript" src="/Scripts/jquery-2.2.2.min.js"></script>
+<link rel="stylesheet" type="text/css"  href="/Content/bootstrap.css" />  
+
+<!-- Links internos  -->
+
+<link href="/Content/Reset.css" rel="stylesheet"/>
+
+
+<!-- Links configurações  -->
+
+<script src="/Scripts/indexAngular.js"></script>
+<script src="/Scripts/indexJq.js"></script>
+<link rel="stylesheet" type="text/css"  href="Views/confirma/content/confirma.css" />
+
